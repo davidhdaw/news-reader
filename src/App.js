e120b27c993c1d40c5aa3b3438b3d2f06b21f230
David Daw <daviddaw@gmail.com>
2022-11-01 16:00:13 -0700

    adds sidebar for routing

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import { Route, Routes } from "react-router-dom";
 import FrontPage from './Components/FrontPage/FrontPage'
+import SideBar from './Components/SideBar/SideBar';
 
 
 function App() {
@@ -8,9 +9,13 @@ function App() {
     <div className="App">
       <h1 className='pageTitle'>New New York Times</h1>
       <p>Literally More News Than is Fit to Print</p>
-      <Routes>
-        <Route exact path="/" element={<FrontPage />} />
-      </Routes>
+      <div className='mainContent'>
+        <SideBar />
+        <Routes>
+          <Route exact path="/" element={<FrontPage frontPage={true}/>} />
+          <Route path="/:id" element={<FrontPage />} />
+        </Routes>
+      </div>
     </div>
   );
 }
--- src/Components/FrontPage/FrontPage.js
+++ src/Components/FrontPage/FrontPage.js
@@ -1,24 +1,49 @@
 import React, {useEffect, useState} from "react";
-import { getTopStories } from "../../apiCalls";
+import { getTopStories, getSectionStories } from "../../apiCalls";
+import { useParams } from "react-router-dom";
 import StoryCard from "../StoryCard/StoryCard";
 import DetailView from '../DetailView/DetailView'
 import './FrontPage.css'
 
-const FrontPage = () => {
+const FrontPage = ({frontPage}) => {
+    let {id} = useParams();
+
+    let section = ''
+
+    if (frontPage === true) {
+        section = 'Top Stories'
+    } else if (id === 'sundayreview') {
+        section = 'Sunday Review'
+    } else if (id === 'us') {
+        section = 'U.S. News'
+    } else {
+        let sectionNameArray = id.split('')
+        sectionNameArray[0] = sectionNameArray[0].toUpperCase()
+        section = sectionNameArray.join('')
+    }
+
     const [topStories, setTopStories] = useState([])
     const [detailView, setDetailView] = useState(false)
     const [detailedStory, setDetailedStory] = useState({})
 
     useEffect(() => {
-        getTopStories().then((data) => {
-            setTopStories(data.results)
-            console.log(topStories)
-        })
-    }, [])
+        if (frontPage === true) {
+            getTopStories().then((data) => {
+                setTopStories(data.results)
+                console.log(topStories)
+            })
+        } else {
+            getSectionStories(id).then((data) => {
+                setTopStories(data.results)
+                console.log(topStories)
+            })
+        }
+    }, [id])
 
     return(
         <div className="frontPage">
         {detailView && <DetailView detailedStory={detailedStory} /> }
+        {!detailView && <h2>{section}</h2>}
         {!detailView && topStories.map((story) => (
             <StoryCard story={story} setDetailView={setDetailView} setDetailedStory={setDetailedStory} />
         ))}
