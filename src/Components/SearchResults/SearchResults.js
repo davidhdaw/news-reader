d1e7367ca106529b4489d54d13b3c78697f71328
David Daw <daviddaw@gmail.com>
2022-11-03 21:57:42 -0700

    various bug fixes

--- src/Components/SearchResults/SearchResults.js
+++ src/Components/SearchResults/SearchResults.js
@@ -8,20 +8,25 @@ const SearchResults = () => {
 let {id} = useParams(); 
 
     const [searchResults, setSearchResults] = useState([])
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
+        setLoading(true)
             getSearchResults(id).then((data) => {
                 setSearchResults(data.response.docs)
+                setLoading(false)
             })
         }, [id])
 
     return(
         <div className="detailedView">
+            {loading && <h2>Loading...</h2>}
             <h1>News Results for: {id}</h1>
             {console.log(searchResults)}
             {searchResults.map((story) => (
             <SearchCard story={story}/>
         ))}
+        {!loading && !searchResults[0] && <h2>Oops. No results found.</h2>}
         </div>
     )
 }
